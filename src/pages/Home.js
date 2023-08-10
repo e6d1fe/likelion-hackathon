b35ec4ddde5c13920ec961adcffb7f8b46509e85
sehyun <e6d1fe@gmail.com>
2023-08-10 14:45:09 +0900

    set up global styles

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,22 @@
+import { styled } from "styled-components";
+
+const PageContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  overflow-x: hidden;
+  overflow-y: scroll;
+`;
+
+const MobileScreen = styled.div`
+  width: 360px;
+`;
+
 function Home() {
-  return <div>Home</div>;
+  return (
+    <PageContainer>
+      <MobileScreen>Home</MobileScreen>
+    </PageContainer>
+  );
 }
 
 export default Home;
