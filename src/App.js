3de933e76f15df62ad8e9fb9fda69c19eabd0b04
sehyun <e6d1fe@gmail.com>
2023-08-13 17:21:19 +0900

    fixed merge conflict

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
-<<<<<<< HEAD
 
-=======
->>>>>>> 08c8d775bfa68dc2df6fac53dd616e3a88f61f7e
 import SearchDestination from "./pages/SearchDestination";
 import SelectDestination from "./pages/SelectDestination";
 import SelectPassengers from "./pages/SelectPassengers";
@@ -15,13 +12,8 @@ function App() {
     <div>
       <Routes>
         <Route path="/" element={<Home />} />
-<<<<<<< HEAD
         <Route path="/destination" element={<SelectDestination />} />
-        <Route path="/searchdestination" element={<SearchDestination />} />     
-=======
         <Route path="/searchdestination" element={<SearchDestination />} />
->>>>>>> 08c8d775bfa68dc2df6fac53dd616e3a88f61f7e
-        <Route path="/destination" element={<SelectDestination />} />
         <Route path="/selectpassengers" element={<SelectPassengers />} />
         <Route path="/complete" element={<Complete />} />
       </Routes>
