cb3e9f9ba2cf94bb0e348bd10af2286a2d471c3f
sehyun <e6d1fe@gmail.com>
2023-08-13 17:28:05 +0900

    changes to fix merge conflict

--- src/pages/SearchDestination.js
+++ src/pages/SearchDestination.js
@@ -80,15 +80,9 @@ const ArriveDes = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-<<<<<<< HEAD
-  font-size: 24px;
-  font-weight: 500;
-  color: #023D6A;
-=======
   font-size: 20px;
   font-weight: 900;
   color: #023d6a;
->>>>>>> 08c8d775bfa68dc2df6fac53dd616e3a88f61f7e
   padding: 11px;
 `;
 
@@ -104,18 +98,15 @@ const SearchContainer = styled.div`
 `;
 
 const WhiteDownArrow = styled.img`
-    margin-top: 0;
+  margin-top: 0;
 `;
 
 const SearchIcon = styled.img`
   padding: 8px 10px 8px 10px;
 `;
 
-<<<<<<< HEAD
-=======
 const DownArrow = styled.div``;
 
->>>>>>> 08c8d775bfa68dc2df6fac53dd616e3a88f61f7e
 const SearchBox = styled.div`
   position: absolute;
   top: 25px;
@@ -126,34 +117,6 @@ const SearchBox = styled.div`
 `;
 
 const SearchInput = styled.input`
-<<<<<<< HEAD
-    flex: 1;
-    border: none;
-    font-size: 14px;
-    color: #999999;
-    width: 100%;
-    outline: none;
-`
-
-const SortStationBox = styled.div`
-    position: relative;
-    width: 100%;
-    height: 24px;
-    background-color: #E5E5E5;
-    display: flex;
-    align-items: center;
-`;
-
-const SortStation = styled.div`
-    position: absolute;
-    left: 10px;
-    color: #185F93;
-    font-size: 13px;
-    font-weight: 600;
-    align-items: center;
-    justify-content: center;
-    text-align: center;
-=======
   flex: 1;
   border: none;
   padding: px;
@@ -179,7 +142,6 @@ const SortStation = styled.div`
   font-weight: 900;
   align-items: center;
   justify-content: center;
->>>>>>> 08c8d775bfa68dc2df6fac53dd616e3a88f61f7e
 `;
 
 const StationNameContainer = styled.div`
@@ -210,16 +172,6 @@ const StationPath = styled.div`
 `;
 
 const StationNameText = styled.div`
-<<<<<<< HEAD
-    position: absolute;
-    left: 15px;
-    top: 12px;
-    display: flex;
-    color: #000000;
-    font-size: 16px;
-    font-weight: 500;
-    align-items: center;
-=======
   position: absolute;
   left: 15px;
   top: 12px;
@@ -227,125 +179,16 @@ const StationNameText = styled.div`
   color: #000000;
   font-size: 16px;
   align-items: center;
->>>>>>> 08c8d775bfa68dc2df6fac53dd616e3a88f61f7e
 `;
 
 const SearchFirstText = styled.div`
-    position: absolute;
-    top: 0;
-    right: 0;
-    z-index: 1;
+  position: absolute;
+  top: 0;
+  right: 0;
+  z-index: 1;
 `;
 
 function SearchDestination() {
-<<<<<<< HEAD
-    return (
-        <PageContainer>
-            <MobileScreen>
-                <Container>
-                    <DepartArriveContainer>
-                        <Depart>출발</Depart>
-                        <img src={roundtriparrow} width="52" height="24" alt="왕복 화살표" />
-                        <Arrive>도착</Arrive>
-                    </DepartArriveContainer>
-
-                    <DepartArriveButtonContainer>
-                        <DepartDesButton type="button">용산</DepartDesButton>
-                        <img src={arrowleft} width="11"alt="화살표" />
-                        <ArriveDes>강릉</ArriveDes>
-                    </DepartArriveButtonContainer>
-
-                    <SearchContainer>
-                       <WhiteDownArrow src={whitedownarrow} width="14px" height="4px" alt="icon" />
-                        <SearchBox>
-                            <SearchIcon src={searchicon} width="20" height="20"/>
-                            <SearchInput type="text" placeholder="역 명의 초성 또는 전체를 입력해주세요."></SearchInput>
-                        </SearchBox>
-                    </SearchContainer>
-
-                    <SortStationBox>
-                        <SortStation>가까운역</SortStation>
-                    </SortStationBox>
-
-                    <StationNameContainer>
-                        <StationName>
-                            <StationNameText>광명</StationNameText>
-                        </StationName>
-                        <StationName>
-                            <StationNameText>영등포</StationNameText>
-                        </StationName>
-                    </StationNameContainer>
-
-                    <SortStationBox>
-                        <SortStation>최근검색구간</SortStation>
-                    </SortStationBox>
-                    <StationPath>
-                        <StationNameText>용산-대전</StationNameText>
-                    </StationPath>
-                    <StationPath>
-                        <StationNameText>서울-강릉</StationNameText>
-                    </StationPath>
-
-                    <SortStationBox>
-                        <SortStation>주요역</SortStation>
-                    </SortStationBox>
-
-                    <StationNameContainer>
-                        <StationName>
-                            <StationNameText>서울</StationNameText>
-                        </StationName>
-                        <StationName>
-                            <StationNameText>용산</StationNameText>
-                        </StationName>
-                    </StationNameContainer>
-
-                    <StationNameContainer>
-                        <StationName>
-                            <StationNameText>광명</StationNameText>
-                        </StationName>
-                        <StationName>
-                            <StationNameText>영등포</StationNameText>
-                        </StationName>
-                    </StationNameContainer>
-
-
-                    <StationNameContainer>
-                        <StationName>
-                            <StationNameText>수원</StationNameText>
-                        </StationName>
-                        <StationName>
-                            <StationNameText>평택</StationNameText>
-                        </StationName>
-                    </StationNameContainer>
-
-
-                    <StationNameContainer>
-                        <StationName>
-                            <StationNameText>대전</StationNameText>
-                        </StationName>
-                        <StationName>
-                            <StationNameText>천안</StationNameText>
-                        </StationName>
-                    </StationNameContainer>
-
-
-                    <StationNameContainer>
-                        <StationName>
-                            <StationNameText>오송</StationNameText>
-                        </StationName>
-                        <StationName>
-                            <StationNameText>조치원</StationNameText>
-                        </StationName>
-                    </StationNameContainer>
-
-                    <SearchFirstText src={searchfirsta} width="24" height="462" alt="검색"/>
-
-
-                </Container>
-            </MobileScreen>
-        </PageContainer>
-    );
-=======
   return (
     <PageContainer>
       <MobileScreen>
@@ -443,7 +286,6 @@ function SearchDestination() {
       </MobileScreen>
     </PageContainer>
   );
->>>>>>> 08c8d775bfa68dc2df6fac53dd616e3a88f61f7e
 }
 
 export default SearchDestination;
