0a4bd302e8a0dd78a23c8e5d3fc32a9587329390
sehyun <e6d1fe@gmail.com>
2023-08-15 21:55:22 +0900

    edited layout for SelectDepart page

--- src/pages/SelectDepart.js
+++ src/pages/SelectDepart.js
@@ -1,8 +1,11 @@
 import { styled } from "styled-components";
 
+import { useNavigate } from "react-router-dom";
+
 import ServiceHeader from "../components/ServiceHeader";
 import ReservationHeader from "../components/ReservationHeader";
 import OneWayHeader from "../components/OneWayHeader";
+import InstructionFooter from "../components/InstructionFooter";
 
 import bottomchevron from "../assets/bottomchevron.svg";
 import arrowleft from "../images/Arrow 1.svg";
@@ -17,7 +20,7 @@ const PageContainer = styled.div`
   height: 100dvh;
   background-color: #f8f8f8;
   height: 100vh;
-  `;
+`;
 
 const MobileScreen = styled.div`
   background-color: #f8f8f8;
@@ -26,14 +29,15 @@ const MobileScreen = styled.div`
 `;
 
 const Container = styled.div`
-  height: 100%;
   padding: 0px 34px;
+  margin-top: 20px;
   display: flex;
   flex-direction: column;
   justify-content: center;
   gap: 10px;
   align-items: center;
 `;
+
 const DepartArriveContainer = styled.div`
   width: 100%;
   height: 27px;
@@ -41,6 +45,7 @@ const DepartArriveContainer = styled.div`
   justify-content: space-around;
   align-items: center;
 `;
+
 const Depart = styled.div`
   display: flex;
   justify-content: center;
@@ -50,6 +55,7 @@ const Depart = styled.div`
   color: #646464;
   margin: 40px;
 `;
+
 const Arrive = styled.div`
   display: flex;
   justify-content: center;
@@ -59,12 +65,14 @@ const Arrive = styled.div`
   color: #646464;
   margin: 40px;
 `;
+
 const DepartArriveButtonContainer = styled.div`
   width: 100%;
   height: 50px;
   display: flex;
   justify-content: space-around;
 `;
+
 const DepartDesButton = styled.div`
   display: flex;
   justify-content: center;
@@ -75,28 +83,29 @@ const DepartDesButton = styled.div`
   color: #023d6a;
   padding: 11px;
 `;
+
 const ArriveDes = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   font-size: 24px;
   font-weight: 500;
-  color: #023D6A;
+  color: #023d6a;
   padding: 11px;
 `;
 
-const Map = styled.div`  
-    width: 310px;
-    height: 28px;
-    color: #646464;
-    border: 1px solid #cdcdcd;
-    border-radius: 40px;
-    background-color: transparent;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    font-size: 16px;
-    font-weight: 500;
+const Map = styled.div`
+  width: 310px;
+  height: 28px;
+  color: #646464;
+  border: 1px solid #cdcdcd;
+  border-radius: 40px;
+  background-color: transparent;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 16px;
+  font-weight: 500;
 `;
 
 const Divider = styled.div`
@@ -105,7 +114,7 @@ const Divider = styled.div`
   align-items: center;
   width: 311px;
   height: 0.5px;
-  background-color: #CDCDCD;
+  background-color: #cdcdcd;
 `;
 
 const InformChooseContainer = styled.div`
@@ -137,65 +146,67 @@ const Choose = styled.div`
   padding: 15px;
 `;
 
-const DownArrow = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 5px;
-  font-weight: 600;
-  color: #646464;
-  padding: 10px;
-`;
-
 function SelectDepart() {
-    return (
-        <PageContainer>
-            <MobileScreen>
-                <ServiceHeader />
-                <ReservationHeader />
-                <OneWayHeader />
-                <Container>
-                    <DepartArriveContainer>
-                        <Depart>출발</Depart>
-                        <img src={roundtriparrow} width="52" height="24" alt="왕복 화살표" />
-                        <Arrive>도착</Arrive>
-                    </DepartArriveContainer>
+  const navigate = useNavigate();
 
-                    <DepartArriveButtonContainer>
-                        <DepartDesButton style={{backgroundColor: "red", color: "white"}}type="button">용산</DepartDesButton>
-                        <img src={arrowleft} width="11" alt="화살표" />
-                        <ArriveDes>강릉</ArriveDes>
-                    </DepartArriveButtonContainer>
+  function toNextPage() {
+    navigate("/selectdepartdetail");
+  }
 
-                    <Map>KTX역 선택 지도</Map>
+  return (
+    <PageContainer>
+      <MobileScreen>
+        <ServiceHeader />
+        <ReservationHeader />
+        <OneWayHeader />
+        <Container>
+          <DepartArriveContainer>
+            <Depart>출발</Depart>
+            <img src={roundtriparrow} width="52" height="24" alt="왕복 화살표" />
+            <Arrive>도착</Arrive>
+          </DepartArriveContainer>
 
-                    <InformChooseContainer>
-                        <Information>출발일</Information>
-                        <Choose>2023년 7월 23일 (일) 21:43</Choose>
-                        <img src={bottomchevron} width="14" height="4" alt="icon" />
-                    </InformChooseContainer>
+          <DepartArriveButtonContainer>
+            <DepartDesButton
+              style={{ backgroundColor: "red", color: "white" }}
+              type="button"
+              onClick={toNextPage}
+            >
+              용산
+            </DepartDesButton>
+            <img src={arrowleft} width="11" alt="화살표" />
+            <ArriveDes>강릉</ArriveDes>
+          </DepartArriveButtonContainer>
 
-                    <Divider></Divider>
+          <Map>KTX역 선택 지도</Map>
 
-                    <InformChooseContainer>
-                        <Information>승객 연령 및 좌석수</Information>
-                        <Choose>총 1명</Choose>
-                        <img src={bottomchevron} width="14" height="4" alt="icon" />
-                    </InformChooseContainer>
+          <InformChooseContainer>
+            <Information>출발일</Information>
+            <Choose>2023년 7월 23일 (일) 21:43</Choose>
+            <img src={bottomchevron} width="14" height="4" alt="icon" />
+          </InformChooseContainer>
 
-                    <Divider></Divider>
+          <Divider></Divider>
 
-                    <InformChooseContainer>
-                        <Information>상세옵션</Information>
-                        <Choose>인접역 표출, SR 연계 표출</Choose>
-                        <img src={bottomchevron} width="14" height="4" alt="icon" />
-                    </InformChooseContainer>
+          <InformChooseContainer>
+            <Information>승객 연령 및 좌석수</Information>
+            <Choose>총 1명</Choose>
+            <img src={bottomchevron} width="14" height="4" alt="icon" />
+          </InformChooseContainer>
 
-                    <Divider></Divider>
+          <Divider></Divider>
 
-                </Container>
-            </MobileScreen>
-        </PageContainer>
-    );
+          <InformChooseContainer>
+            <Information>상세옵션</Information>
+            <Choose>인접역 표출, SR 연계 표출</Choose>
+            <img src={bottomchevron} width="14" height="4" alt="icon" />
+          </InformChooseContainer>
+
+          <Divider></Divider>
+        </Container>
+        <InstructionFooter />
+      </MobileScreen>
+    </PageContainer>
+  );
 }
 export default SelectDepart;
